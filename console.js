e7dd0c6ef90c20b13fe43a274f3d6041cd74adbc
Konrad Bieniek <konradbieniek@gmail.com>
2021-12-30 00:11:18 +0100

    console lesson

---
+++ console.js
@@ -0,0 +1,19 @@
+// console.log(window.console);
+
+console.warn("warning");
+console.error('error')
+
+console.table(['123', 123])
+
+const persons = [
+    {
+        name: 'Lisa',
+        age: 23
+    },
+    {
+        name: 'Tokyo',
+        age: 33
+    }
+]
+
+console.table(persons);
