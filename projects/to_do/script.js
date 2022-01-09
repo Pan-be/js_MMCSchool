8f4fd3c4abdf8e27d56ec7ddde0c40477dcaa2d7
Konrad Bieniek <konradbieniek@gmail.com>
2022-01-09 22:18:20 +0100

    adding Tools Area to Todo

--- projects/to_do/script.js
+++ projects/to_do/script.js
@@ -2,6 +2,11 @@ let todoInput // <-- miejsce na treść todo
 let errorInfo // <-- paragraf z info o braku zadań/tekstu
 let addBtn // <-- przycisk Add - dodanie elementu
 let ulList // <-- list zadań
+let newTodo // <--- nowe zadanie
+// let newToolslArea // <-- nowe pole z narzędziami
+// let newCompleteButton // <--  nowy przycisk zrobione
+// let newEditButton // <--  nowy przycisk edycji
+// let newDeleteButton // <--  nowy przycisk kasowania
 
 const main = () => {
     prepareDOMElements()
@@ -16,15 +21,42 @@ const prepareDOMElements = () => {
 }
 
 const prepareDOMEvents = () => {
-    addBtn.addEventListener('click', addNewTask)
+    addBtn.addEventListener('click', addNewTodo)
 }
 
-const addNewTask = () => {
+const addNewTodo = () => {
     if (todoInput.value !== '') {
-        console.log('ok');
+        newTodo = document.createElement('li')
+        newTodo.textContent = todoInput.value
+        createToolsArea()
+        
+        ulList.append(newTodo);
+
+        todoInput.value = ''
+        errorInfo.textContent = ''
     } else {
         errorInfo.textContent = 'Wpisz treść zadania.'
     }
+}
+
+const createToolsArea = () => {
+    const newToolsArea = document.createElement('div')
+    newToolsArea.classList.add('tools')
+    newTodo.append(newToolsArea)
+
+    const newCompleteButton = document.createElement('button')
+    newCompleteButton.classList.add('complete')
+    newCompleteButton.innerHTML = '<i class="fas fa-check"></i>'
+
+    const newEditButton = document.createElement('button')
+    newEditButton.classList.add('edit')
+    newEditButton.textContent = 'EDIT'
+
+    const newDeleteButton = document.createElement('button')
+    newDeleteButton.classList.add('delete')
+    newDeleteButton.innerHTML = '<i class="fas fa-times"></i>'
+
+    newToolsArea.append(newCompleteButton, newEditButton, newDeleteButton)
 }
 
 document.addEventListener('DOMContentLoaded', main)
