b98f66050485853d771a0b8b311bcf4489868fc2
Konrad Bieniek <konradbieniek@gmail.com>
2022-01-20 10:25:55 +0100

    a lesson about prototype in js

---
+++ objects/prototype.js
@@ -0,0 +1,17 @@
+function User(name, age) {
+    this.name = name
+    this.age = age
+    this.hello = function () {
+        console.log(`Cześć ${this.name}`);
+    }
+}
+
+const newUser = new User('Janusz', 60)
+const newUser2 = new User('Sławomir', 48)
+const newUser3 = new User('Bogdan', 61)
+
+User.prototype.hello = function () {
+    console.log(`Cześć ${this.name}`);
+}
+
+newUser3.hello()
